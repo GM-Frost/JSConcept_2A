e03b31fa3f22f11a20d8672769e225deca77ed28
Nayan Bastola <nayanbastola777@gmail.com>
2023-07-05 16:06:14 -0400

    Use For Loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
 function App() {
-  function calculateAverageGrade(students) {
-    const totalGrades = students.grade.reduce((total, grade) => total + grade);
-    const averageGrades = totalGrades / students.grade.length;
-    return averageGrades;
-  }
-
   const students = [
     {
       name: "Arata",
@@ -23,24 +17,44 @@ function App() {
     },
   ];
 
-  function printStudentInfo(students) {
-    return (
-      <>
-        <div>
-          The Student Name is: <span className="name">{students.name}</span>
-        </div>
-        <div>
-          The Student's age: <span className="age">{students.age}</span>
-        </div>
-        <div>
-          The Student Average Grade is:
-          <span className="average"> {calculateAverageGrade(students)}</span>
-        </div>
-      </>
-    );
+  function calculateAverageGrade(students) {
+    const totalGrades = students.grade.reduce((total, grade) => total + grade);
+    const averageGrades = totalGrades / students.grade.length;
+    return averageGrades;
   }
 
-  return printStudentInfo(students[1]);
-}
+  function printStudentInfo() {
+    const allStudents = [];
 
+    students.forEach((student, index) => {
+      allStudents.push(
+        <div
+          key={index}
+          style={{
+            width: "50%",
+            textAlign: "center",
+            marginBottom: "10px",
+            borderRadius: "8px",
+            padding: "5px",
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+          }}
+        >
+          <h2>
+            Name:
+            <span style={{ color: "blue" }}>{student.name}</span>
+          </h2>
+          <p>Age: {student.age}</p>
+          <h3>
+            Average Grade:
+            <span style={{ color: "green" }}>
+              &nbsp;{calculateAverageGrade(student)}
+            </span>
+          </h3>
+        </div>
+      );
+    });
+    return <div>{allStudents}</div>;
+  }
+  return printStudentInfo();
+}
 export default App;
